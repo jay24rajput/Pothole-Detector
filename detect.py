267820d35767323b65bf2debf05c81a1da0b1b02
Jay Rajput <jay24rajput@gmail.com>
2019-02-18 23:06:37 +0530

    Added output file

--- detect.py
+++ detect.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=cv2.imread('11.jpg',cv2.IMREAD_COLOR)
+img=cv2.imread('pothole.jpg',cv2.IMREAD_COLOR)
 img = cv2.resize(img, (500, 500))
 
 # img=cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
