1e7790cd8d46f96e5205eb0431effd8ef66228a4
Jay Rajput <jay24rajput@gmail.com>
2020-05-16 12:03:11 +0530

    Re-testing the commit functionality with signed comment.
    Comments removed for re-testing the git commit functionality.
    In this commit the vscode plugin should not ask the user to signing to github again.
    Lets test the validity of this functionality.
    
    Signed-off-by: Jay Rajput <jay24rajput@gmail.com>

--- detect.py
+++ detect.py
@@ -10,7 +10,6 @@ blur = cv2.GaussianBlur(gray,(5,5),30)
 
 ret,thresh = cv2.threshold(blur,125,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 _,contours,_= cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# _,contours,_= cv2.findContours(thresh,1, 2)
 _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 for contour in contours:
@@ -22,7 +21,6 @@ for contour in contours:
 
 cv2.imshow('otsu',thresh)
 cv2.imshow('img',img)
-# cv2.imshow('res',res)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
